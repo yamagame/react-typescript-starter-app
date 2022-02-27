6b1e136c2b9343c935d0afc194abd07e2e8cb568
yamagame <yamagame@mac.com>
2022-02-28 00:20:48 +0900

    add dirs

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path/posix';
+import fs from 'fs/promises';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    hmr: { overlay: false },
-    proxy: {
-      '/pages': 'http://localhost:4000',
+export default defineConfig(async ({ command, mode }) => {
+  const dirs = (await fs.readdir('./src', { withFileTypes: true }))
+    .filter((dirent) => dirent.isDirectory())
+    .reduce((sum, dir) => {
+      sum[dir.name] = path.resolve(__dirname, path.join('src', dir.name));
+      return sum;
+    }, {});
+  return {
+    plugins: [react()],
+    server: {
+      hmr: { overlay: false },
+      proxy: {
+        '/pages': 'http://localhost:4000',
+      },
     },
-  },
-  resolve: {
-    alias: {
-      App: path.resolve(__dirname, 'src/App'),
-      app: path.resolve(__dirname, 'src/app'),
-      pages: path.resolve(__dirname, 'src/pages'),
-      features: path.resolve(__dirname, 'src/features'),
+    resolve: {
+      alias: {
+        App: path.resolve(__dirname, 'src/App'),
+        ...dirs,
+      },
     },
-  },
+  };
 });
