df16cffb7a7af0058f6a5dda2f0265e6b5034658
yamagame <yamagame@mac.com>
2022-03-06 09:33:20 +0900

    add ToDoAdapter

--- src/routers/ApplicationRouter.tsx
+++ src/routers/ApplicationRouter.tsx
@@ -5,6 +5,7 @@ import { FormAdapter } from 'ui/adapters/FormAdapter';
 import { FormFormikAdapter } from 'ui/adapters/FormFormikAdapter';
 import { ArticleAdapter } from 'ui/adapters/ArticleAdapter';
 import { MyPage } from 'ui/components/pages/MyPage';
+// import { ToDoAdapter } from 'ui/adapters/ToDoAdapter';
 
 export function AppRouter() {
   return (
@@ -15,13 +16,15 @@ export function AppRouter() {
         {/*-------------- カウンターページ */}
         <Route path="/counter" element={<CounterAdapter />} />
         {/*-------------- フォームページ */}
-        <Route path={'form'} element={<FormAdapter />} />
+        <Route path={'/form'} element={<FormAdapter />} />
         {/*-------------- フォーミックフォームページ */}
-        <Route path={'form-formik'} element={<FormFormikAdapter />} />
+        <Route path={'/form-formik'} element={<FormFormikAdapter />} />
         {/*-------------- マイページ */}
-        <Route path={'my-page'} element={<MyPage />} />
+        <Route path={'/my-page'} element={<MyPage />} />
+        {/*-------------- ToDOページ */}
+        {/* <Route path={'/todo'} element={<ToDoAdapter />} /> */}
         {/*-------------- 各ページ */}
-        <Route path={'article/:pageId'} element={<ArticleAdapter />} />
+        <Route path={'/article/:pageId'} element={<ArticleAdapter />} />
       </Routes>
     </BrowserRouter>
   );
